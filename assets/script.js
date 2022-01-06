ad862f0b077a32cd167541ba13ea5347bd19715e
KW <kenneth_sl_wong@msn.com>
2022-01-06 16:28:14 -0500

    added character selection section, added selected characters to array

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,9 @@ var generatePassword = function() {
 
   var passwordLength = "";
 
+
   //Password Length//
+
 
   //Ask if user wants to specify length (with default 8)
   var lengthConfirm = window.confirm("Would you like to specify length of password?");
@@ -48,25 +50,48 @@ var generatePassword = function() {
     window.alert("Your length has been set to the default of 8.");
   };
 
+
   //Password Characters//
+
 
   //Loop for characters, condition to check if 1 is selected
   var charactersCheck = false;
-  while(charactersCheck === false) {
+  var charactersArray = [];
+
+  while(!charactersCheck) {
 
     //Ask user if they want lowercase
+    if(window.confirm("Do you want to use lowercase letters in your password?")){
+      charactersArray.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+      console.log("Password populated with lowercase.");
+      charactersCheck = true;
+    }
 
     //Ask user if they want uppercase
+    if(window.confirm("Do you want to use uppercase letters in your password?")){
+      charactersArray.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+      console.log("Password populated with uppercase.");
+      charactersCheck = true;
+    }
 
     //Ask user if they want numeric
+    if(window.confirm("Do you want to use numbers in your password?")){
+      charactersArray.push("0","1","2","3","4","5","6","7","8","9");
+      console.log("Password populated with numbers.");
+      charactersCheck = true;
+    }
 
     //Ask user if they want special characters
-
+    if(window.confirm("Do you want to use lower case letters in your password?")){
+      charactersArray.push(" ","!",'"',"#","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~");
+      console.log("Password populated with symbols.")
+      charactersCheck = true;
+    }
+    
+    if(!charactersCheck){
+      window.alert("You have to select at least one character type. Please try again.");
+    }
   }
-
-  //Initialize array
-
-  //Populate array based on if statements
 
   //Loop and use math function to populate array
 }
